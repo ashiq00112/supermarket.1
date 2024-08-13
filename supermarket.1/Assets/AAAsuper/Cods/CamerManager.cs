7b68b0a803a2bc1a6c048f6e6b5276be43712440
ashiq00112 <ashi26635@gmail.com>
2024-08-13 14:41:53 +0530

    enna sonna

--- supermarket.1/Assets/AAAsuper/Cods/CamerManager.cs
+++ supermarket.1/Assets/AAAsuper/Cods/CamerManager.cs
@@ -9,17 +9,12 @@ public class CamerManager : MonoBehaviour
 
     public DOTweenAnimation positionAnimation;
     public DOTweenAnimation rotationAnimation;
- 
+    public DOTweenAnimation moneybox;
     public float rotateDuration = 1.0f;
-    public Vector3 Registerposition;
-    public Vector3 Registerrotation;
     public float moveDuration = 1.0f;
-    public Vector3 cammainposiiton;
-    public Vector3 cammainrotation;
     //cardpaycamera
-    public Vector3 cardpayposition;
-    public Vector3 cardpayrotation;
-
+    
+   
     public bool toog;
 
 
@@ -29,14 +24,18 @@ public class CamerManager : MonoBehaviour
     {
         if (toog)
         {
-            positionAnimation.transform.DOMove(Registerposition, moveDuration);
-            rotationAnimation.transform.DORotate(Registerrotation, 1.0f);
+            positionAnimation.transform.DOMove(new Vector3(20.478f, 2.9f, 1.428f), moveDuration);
+            rotationAnimation.transform.DORotate(new Vector3(42.39f, 180f, 0f), 1.0f);
+            moneybox.transform.DOMove(new Vector3 (20.376f, 1.484f, -0.034f), moveDuration);
             toog = false;
         }
         else {
-            positionAnimation.transform.DOMove(cammainposiiton, moveDuration);
-            rotationAnimation.transform.DORotate(cammainrotation, 1.0f);
+            positionAnimation.transform.DOMove(new Vector3(18.324f, 2.813f, 2.165f), moveDuration);
+            rotationAnimation.transform.DORotate(new Vector3(12.922f, 120.791f, 0.158f), 1.0f);
+            moneybox.transform.DOMove(new Vector3(20.376f, 1.484f, -0.794f), moveDuration);
             toog = true;
+
+
 
         }
 
@@ -46,18 +45,18 @@ public class CamerManager : MonoBehaviour
         if (toog)
         {
 
-            positionAnimation.transform.DOMove(cardpayposition, moveDuration);
-            rotationAnimation.transform.DORotate(cardpayrotation, 1.0f);
+            positionAnimation.transform.DOMove(new Vector3(21.435f, 2.42f, 0.448f), moveDuration);
+            rotationAnimation.transform.DORotate(new Vector3(11.485f, 84.845f, -1.565f), 1.0f);
             toog = false;
         }
         else
         {
-            positionAnimation.transform.DOMove(cammainposiiton, moveDuration);
-            rotationAnimation.transform.DORotate(cammainrotation, 1.0f);
+            positionAnimation.transform.DOMove(new Vector3(18.324f, 2.813f, 2.165f), moveDuration);
+            rotationAnimation.transform.DORotate(new Vector3(12.922f, 120.791f, 0.158f), 1.0f);
             toog = true;
         }
 
 
         }
-
+    
 }
